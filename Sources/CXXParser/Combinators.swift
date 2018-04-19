b4a73ded3e35b08f323f328be37b6ed3bfbcbd02
Donovan Crichton <donovan.crichton@griffithuni.edu.au>
2018-04-19 12:35:46 +1000

    Updated combinators.

--- Sources/CXXParser/Combinators.swift
+++ Sources/CXXParser/Combinators.swift
@@ -1,61 +1,84 @@
-infix operator <|>: AdditionPrecedence
+infix operator <|>: AdditionPrecedence // Alternative
+infix operator >>=: LogicalDisjunctionPrecedence // Bind
+infix operator <^>: AdditionPrecedence // Fmap
+infix operator <*>: AdditionPrecedence // Apply
+infix operator *> : AdditionPrecedence // Right Apply
+infix operator <* : AdditionPrecedence // Left Apply
 
 
-// The Parser datatype 
+
+/**
+ * The Parser structure.
+ *
+ * A Parser is just a wrapper around a parse function: `String -> (T, String)?`
+ */
 struct Parser<T> {
-  let parse: (String) -> [(T, String)]
+  
+  let parse: (String) -> (T, String)?
 }
 
-// The 'run' function applies Parser p to String s and returns an optional T if 
-// successful or nil otherwise.
-func run<T>(p: Parser<T>, s: String) -> T? {
-  return nil
+func id<T>(_ t: T) -> T {
+  return t
 }
 
-// The 'between' function checks for the sequential occurance of
-// start p end. That is, that parser p is between parser start and parser end.
-func between<T, U, V>(start: Parser<T>, end: Parser<U>, p: Parser<V>) 
-  -> Parser<V>? {
-  return nil
+func const<T, U>(_ t: T) -> (U) -> T {
+  return { _ in t }
 }
 
-// The 'failure' combinator returns an empty Parser.
-func failure<T>() -> Parser<T> {
-  return Parser<T> { _ in [] }
+// The 'pure' combinator lifts a T up into a Parser<T>.
+func pure<T>(_ t: T) -> Parser<T> {
+  return Parser { s in (t, s) }
 }
 
-// The '<|>' operator tries to first parse p, if p fails, it tries to parse q.
-func <|> <T>(p: Parser<T>, q: Parser<T>) -> Parser<T> {
-  return Parser<T> {s in
-    let r = p.parse(s)
-    return r.isEmpty ? q.parse(s) : r
-  }
+// The 'empty' combinator represents a blank or failed Parser.
+func empty<T>() -> Parser<T> {
+  return Parser { _ in nil }
 }
 
-// The 'some' function applies the Parser p one or more times in succession.
-// On failure it terminates.
-func some<T>(p: Parser<T>) -> Parser<[T]> {
-  var xs = []
-  func f(s: String) -> [(T, String)] {
-    let r = p.parse(s)
-    if let (a, s') = r.first {
-      f(s')
+// pronounced "bind". The '>>=' function allows sequential chaining of parsers.
+func >>= <T, U>(_ p: Parser<T>, _ f: @escaping (T) -> Parser<U>) -> Parser<U> {
+  return Parser {s in
+    if let (a, s2) = p.parse(s) {
+      return f(a).parse(s2)
+    }
+    else {
+      return nil
     }
   }
 }
 
-// The 'pure' combinator constructs a Parser<T> from a T.
-func pure<T>(t: T) -> Parser<T> {
-  return Parser<T> { s in [(t, s)] }
+// pronounced "fmap". The '<^>' function allows transformations of parsers T to
+// parsers U, given a function T -> U.
+func <^> <T, U>(_ f: @escaping (T) -> U, p: Parser<T>) -> Parser<U> {
+  return p >>= { s in pure(f(s)) }
 }
 
+// pronounced "alternative". The '<|>' function attempts parser p, and if it
+// fails, then attempts parser q.
+func <|> <T>(_ p: Parser<T>, _ q: Parser<T>) -> Parser<T> {
+  return Parser {s in
+    if let (a, s2) = p.parse(s) {
+      return (a, s2)
+    }
+    else {
+      return q.parse(s)
+    }
+  }
+}
 
+// prounounced "apply". The '<*>' function takes a Parser (T -> U) and some
+// Parser T, and returns the Parser U from the application of T to T -> U.
+func <*> <T, U>(_ p: Parser<(T) -> U>, _ q: Parser<T>) -> Parser<U> {
+  return p >>= { f in f <^> q }
+}
 
-// func bind<T, U>(p: Parser<T>, f: T -> Parser<U>) -> Parser<U> {
-//   need concatMap to go farther.
-// }
+func <* <T, U>(_ p: Parser<T>, _ q: Parser<U>) -> Parser<T> {
+  return const <^> p <*> q
+}
 
-
+func *> <T, U>(_ p: Parser<T>, _ q: Parser<U>) -> Parser<U> {
+  return const(id) <^> p <*> q
+}
 
 
 
