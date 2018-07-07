3c95845b1f15dff15d0960fee914d63952de3548
Callum McColl <callum.mccoll1@gmail.com>
2018-07-07 15:20:47 +1000

    Fixed Controller Package.swift

--- machines/Controller/.build/ControllerMachine/Package.swift
+++ machines/Controller/.build/ControllerMachine/Package.swift
@@ -1,17 +1,22 @@
+// swift-tools-version:4.0
 import PackageDescription
-let packages = Package(
-    name: "ControllerMachine",
-    dependencies: [
-        .Package(url: "ssh://git.mipal.net/git/CGUSimpleWhiteboard", majorVersion: 1),
-        .Package(url: "%machines_tests%/PingPong.machine/.build/PingPongMachine", majorVersion: 0),
-        .Package(url: "%machines_tests%/Controller.machine/.build/ControllerMachineBridging", majorVersion: 0)
-    ]
-)
 
-products.append(
-    Product(
-        name: "ControllerMachine",
-        type: .Library(.Dynamic),
-        modules: ["ControllerMachine"]
-    )
+let package = Package(
+    name: "ControllerMachine",
+    products: [
+        .library(
+            name: "ControllerMachine",
+            type: .dynamic,
+            targets: ["ControllerMachine"]
+        )
+    ],
+    dependencies: [
+        .package(url: "ssh://git.mipal.net/git/CGUSimpleWhiteboard", .branch("swift-4.2")),
+        .package(url: "ssh://git.mipal.net/git/swift_wb", .branch("swift-4.2")),
+        .package(url: "%machines_tests%/PingPong.machine/.build/PingPongMachine/", .branch("master")),
+        .package(url: "%machines_tests%/Controller.machine/.build/ControllerMachineBridging/", .branch("master"))
+    ],
+    targets: [
+        .target(name: "ControllerMachine", dependencies: ["GUSimpleWhiteboard", "PingPongMachine"])
+    ]
 )
