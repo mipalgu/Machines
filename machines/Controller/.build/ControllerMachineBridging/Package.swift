5d51a42d5d5e3f4a3ec9ea236b54f9157cb4df10
Callum McColl <callum.mccoll1@gmail.com>
2018-07-07 15:42:36 +1000

    Fixed ControllerMachineBridging Package.swift - all tests now pass

--- machines/Controller/.build/ControllerMachineBridging/Package.swift
+++ machines/Controller/.build/ControllerMachineBridging/Package.swift
@@ -1,7 +1,12 @@
-// swift-tools-version:3.1
+// swift-tools-version:4.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-    name: "ControllerMachineBridging"
+    name: "ControllerMachineBridging",
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ]
 )
