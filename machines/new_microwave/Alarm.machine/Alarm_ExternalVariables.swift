5c52313773a7e3c8ef274f30fe903daac702132e
Callum McColl <callum.mccoll1@gmail.com>
2020-07-05 00:04:12 +1000

    Changed Alarm so that it now uses new sensors/actuators

--- machines/new_microwave/Alarm.machine/Alarm_ExternalVariables.swift
+++ machines/new_microwave/Alarm.machine/Alarm_ExternalVariables.swift
@@ -1,1 +1,3 @@
-var microwave_status: WhiteboardVariable<MicrowaveStatus> = WhiteboardVariable(msgType: kMicrowaveStatus_v)
+let doorOpen: WhiteboardVariable<Bool> = WhiteboardVariable(msgType: kdoorOpen_v)
+let timeLeft: WhiteboardVariable<Bool> = WhiteboardVariable(msgType: ktimeLeft_v)
+sink sound: WhiteboardVariable<Bool> = WhiteboardVariable(msgType: ksound_v)
--- machines/new_microwave/Alarm.machine/Alarm_Vars.swift
+++
@@ -1,2 +0,0 @@
-
-var sound: Bool = false
