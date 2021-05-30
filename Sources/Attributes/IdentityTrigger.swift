cffd0b78fd59f0725af8262026bf2c4e6cc28bcc
Morgan McColl <morgan.mccoll@alumni.griffithuni.edu.au>
2021-05-31 01:11:21 +1000

    Made sync trigger.

--- Sources/Attributes/IdentityTrigger.swift
+++ Sources/Attributes/IdentityTrigger.swift
@@ -15,8 +15,32 @@ public struct IdentityTrigger<Path: PathProtocol>: TriggerProtocol {
         self.path = path
     }
     
+    public func sync<TargetPath: PathProtocol>(target: TargetPath) -> SyncTrigger<Path, TargetPath> where TargetPath.Root == Root, TargetPath.Value == Path.Value {
+        SyncTrigger(source: path, target: target)
+    }
+    
     public func performTrigger(_ root: inout Path.Root) -> Result<Bool, AttributeError<Path.Root>> {
         .success(false)
+    }
+    
+}
+
+public struct SyncTrigger<Source: PathProtocol, Target: PathProtocol>: TriggerProtocol where Source.Root == Target.Root, Source.Value == Target.Value {
+    
+    public typealias Root = Source.Root
+    
+    let source: Source
+    
+    let target: Target
+    
+    public init(source: Source, target: Target) {
+        self.source = source
+        self.target = target
+    }
+    
+    public func performTrigger(_ root: inout Source.Root) -> Result<Bool, AttributeError<Source.Root>> {
+        root[keyPath: target.path] = root[keyPath: source.keyPath]
+        return .success(true)
     }
     
 }
