274c739d7fca668a9555c791e47502610d5e8541
Callum McColl <callum.mccoll1@gmail.com>
2021-07-15 12:05:15 +1000

    Don't depend on macos version 10.15

--- Package.swift
+++ Package.swift
@@ -5,7 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "Machines",
-    platforms: [.macOS(.v10_15)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(name: "Machines", targets: ["Machines", "CLFSMMachines", "CXXBase", "CXXMachines", "SpartanFSMMachines", "SwiftMachines", "UCFSMMachines", "VHDLMachines"])
