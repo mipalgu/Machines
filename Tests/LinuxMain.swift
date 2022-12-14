73b2c10c4c8eab0fbd78ec2a07352c9d37b0fde7
Morgan McColl <morgan.mccoll@griffithuni.edu.au>
2022-12-14 21:54:20 +1000

    Made linux autodetect tests.

--- Tests/LinuxMain.swift
+++
@@ -1,9 +0,0 @@
-import XCTest
-
-import SwiftMachinesTests
-
-XCTMain([
-    testCase(MachineAssemblerTests.allTests),
-    // testCase(MachineCompilerTests.allTests),
-    testCase(MachineParserTests.allTests)]
-)
