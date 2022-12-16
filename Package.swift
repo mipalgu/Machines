bae8df9ea1744aae584ed2a8696566aa64b6eae3
Morgan McColl <morgan.mccoll@griffithuni.edu.au>
2022-12-16 15:32:00 +1000

    Changed swift_helpers version to 1.0.1.

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(name: "swift_helpers", url: "https://github.com/mipalgu/swift_helpers.git", from: "1.0.0")
+        .package(name: "swift_helpers", url: "https://github.com/mipalgu/swift_helpers.git", from: "1.0.1"),
         .package(name: "MetaLanguage", url: "ssh://git@github.com/Morgan2010/MetaLanguage.git", .branch("main")),
         .package(url: "git@github.com:mipalgu/GUUnits", from: "2.0.1")
     ],
